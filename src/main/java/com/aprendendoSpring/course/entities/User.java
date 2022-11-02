6afb6cfaf3c302694f467399f9772d6a3ac51169
Natanael Nogueira <102445138+natanaelnogueira17@users.noreply.github.com>
2022-11-02 10:46:45 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/aprendendoSpring/course/entities/User.java
+++ src/main/java/com/aprendendoSpring/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.aprendendoSpring.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 
@@ -25,7 +28,12 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client") // aqui fala que esse atributo esta mapeado por client
+	private List<Order> orders = new ArrayList<>();
 	
+	
+	
+
 	public User() {
 		super();
 	}
@@ -67,6 +75,10 @@ public class User implements Serializable{
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 	@Override
 	public int hashCode() {
