9cfd8eb05228444671e8133fdcdb9f3f05120d25
Natanael Nogueira <102445138+natanaelnogueira17@users.noreply.github.com>
2023-01-08 13:38:50 -0300

    Exception handling - delete

--- course/src/main/java/com/aprendendoSpring/course/resources/exceptions/ResourceExceptionHandler.java
+++ course/src/main/java/com/aprendendoSpring/course/resources/exceptions/ResourceExceptionHandler.java
@@ -9,10 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.aprendendoSpring.course.services.exceptions.DatabaseException;
 import com.aprendendoSpring.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
+	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError>RecourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		String error = "Resource not found ";
@@ -21,4 +23,12 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status).body(err);
 	}
 	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError>dataBase(DatabaseException e, HttpServletRequest request){
+		String error = "Resource not found ";
+		HttpStatus status  =  HttpStatus.BAD_REQUEST;
+		StandardError err =  new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
 }
